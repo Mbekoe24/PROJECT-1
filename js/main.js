3b80fc7af3f3577bb7b609760c5e3be735695993
Michael Bekoe <thebestman@Michaels-MacBook-Pro-4.local>
2020-03-09 17:46:33 -0400

    fixed js bug

--- js/main.js
+++ js/main.js
@@ -2,35 +2,31 @@
 //=====================================
 //=========== global variables ==========
 //=====================================
-
+let randomNum = Math.floor(Math.random() * 807);
+// console.log(randomNum);
 const pokeData = {
   url: "https://pokeapi.co/api/v2/",
   type: "pokemon",
-  id: "54"
+  id: randomNum
 };
-
 const { url, type, id } = pokeData;
 const pokeUrl = `${url}${type}/${id}`;
-
+const pokeImg = document.querySelector("#pokemon-img");
 // console.log(pokeUrl);
 // // =======================
 // /// === Pokemon ======
 // //========================
-//
-const pokeImg = document.querySelector("#pokemon-img");
 
 const getPokemon = async () => {
   const response = await axios.get(pokeUrl);
   // console.log(response.data.id);
 };
-
 getPokemon();
 
 // //=======================
-// //== image get request
+// //== image/ name get request
 // //=======================
 
-//
 const pokeHTML = async () => {
   const response = await axios.get(pokeUrl);
   const innerHTML = `
@@ -43,31 +39,15 @@ const pokeHTML = async () => {
 
 pokeHTML();
 
-const pokeList = async () => {
-  const response = await axios.get(pokeUrl);
-  let currentIndex = pokeData[id['']];
-  newIndex = [];
-  for (i = 0; i < currentIndex; i++) {
-    Math.floor(Math.random()) * currentIndex.parsInt().toString + 1;
-  }
-  console.log(newIndex);
-  // console.log(response.data);
-};
-
-pokeList();
-
-// buildQuestion = () => {};
-
-// let pokemonArr = pokeData(id);
-
-// const pokemonRandomizer = pokemonArr => {
-//   for (let i = 0; i < pokemonArr[id].length; i++) {
-//     Math.floor(Math.random()) * pokemonArr.length;
+// const pokeList = async () => {
+//   const response = await axios.get(pokeUrl);
+//   let currentIndex = pokeData[id[""]];
+//   newIndex = [];
+//   for (i = 0; i < currentIndex; i++) {
+//     Math.floor(Math.random()) * currentIndex.parsInt() + 1;
 //   }
-//   return pokemonArr;
+//   console.log(newIndex);
+//   // console.log(response.data);
 // };
-// pokemonRandomizer();
 
-// for (let i = 0; i < response.data.id.length; i++) {
-//   Math.floor(Math.random()) * response.data.id.length;
-// }
+// pokeList();
