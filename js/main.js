d7cd680757755c44cb4e675d033e6ef234d39fdf
Michael Bekoe <thebestman@Michaels-MacBook-Pro-4.local>
2020-03-10 17:00:40 -0400

    fixing up js

--- js/main.js
+++ js/main.js
@@ -1,59 +1,36 @@
-// Axios calls will need to be formatted like this:
-//=====================================
-//=========== global variables ==========
-//=====================================
-
-let randomNum = Math.floor(Math.random() * 807);
-// console.log(randomNum);
-const pokeData = {
-  url: "https://pokeapi.co/api/v2/",
-  type: "pokemon",
-  id: randomNum
-};
-const { url, type, id } = pokeData;
-const pokeUrl = `${url}${type}/${id}`;
-const pokeImg = document.querySelector("#pokemon-img");
-// console.log(pokeUrl);
 // // =======================
 // /// === Pokemon ======
 // //========================
-
-const getPokemon = async () => {
-  const response = await axios.get(pokeUrl);
-  // console.log(response.data.id);
+const makeRandomPoke = () => {
+  let randomNum = Math.floor(Math.random() * 807);
+  const pokeData = {
+    url: "https://pokeapi.co/api/v2/",
+    type: "pokemon",
+    id: randomNum
+  };
+  const { url, type, id } = pokeData;
+  const pokeUrl = `${url}${type}/${id}`;
+  return pokeUrl;
 };
-getPokemon();
+let pokeUrl = makeRandomPoke();
+const pokeImg = document.querySelector("#pokemon-img");
 
 // //=======================
 // //== image/ name get request
 // //=======================
 
 const pokeHTML = async () => {
-  const response = await axios.get(pokeUrl);
-  const innerHTML = `
-  <img src= ${response.data.sprites.front_default}>
-  `;
-  const innerHTML2 = `
-  <div class="answer1">${response.data.name}</div>
-  `;
-  const pokeInfo = document.querySelector("#pokemon-img");
-  pokeInfo.innerHTML = innerHTML;
+  const response = await axios.get(makeRandomPoke());
+  const pokeInfo = document.querySelector("#pokemon-img img");
+  pokeInfo.src = response.data.sprites.front_default;
   const pokeButton = document.querySelector(".answer1");
   pokeButton.innerText = response.data.name;
 };
-
 pokeHTML();
 /// second instance of random name
-let randomNum2 = Math.floor(Math.random() * 807);
-const pokeData2 = {
-  url: "https://pokeapi.co/api/v2/",
-  type: "pokemon",
-  id: randomNum2
-};
-const pokeUrl2 = `${pokeData2.url}${pokeData2.type}/${pokeData2.id}`;
 
 const randomNames2 = async () => {
-  const response = await axios.get(pokeUrl2);
+  const response = await axios.get(makeRandomPoke());
   const innerHTML = `${response.data.name}`;
   const random2 = document.querySelector(".answer2");
   random2.innerHTML = innerHTML;
@@ -61,16 +38,9 @@ const randomNames2 = async () => {
 randomNames2();
 
 /// third instance of random name
-let randomNum3 = Math.floor(Math.random() * 807);
-const pokeData3 = {
-  url: "https://pokeapi.co/api/v2/",
-  type: "pokemon",
-  id: randomNum3
-};
-const pokeUrl3 = `${pokeData3.url}${pokeData3.type}/${pokeData3.id}`;
 
 const randomNames3 = async () => {
-  const response = await axios.get(pokeUrl3);
+  const response = await axios.get(makeRandomPoke());
   const innerHTML = `${response.data.name}`;
   const random3 = document.querySelector(".answer3");
   random3.innerHTML = innerHTML;
@@ -93,27 +63,73 @@ randomizeButton();
 
 // Logic for the correct answer === userInput
 
+let correctAns = document.querySelector(".answer1");
+let clickPokemon = document.querySelectorAll(".answer-button");
+let scoreBoard = document.querySelector("score-button");
+let revealPic = (document.querySelector("#pokemon-img").style.brightness =
+  "100%");
+tempScore = scoreBoard;
+let userScore = 0;
+let tempScore;
+for (let i = 0; i < clickPokemon.length; i++) {
+  clickPokemon[i].addEventListener("click", () => {
+    pokeHTML();
+    randomNames2();
+    randomNames3();
+    randomizeButton();
+    revealPic;
+    tempScore = userScore.parsInt()++;
+    if (clickPokemon[i] === correctAns) {
+      userScore++;
+
+      // alert(
+      //   `You got it right, you gotten  ${userScore} question right so far!`
+      // );
+    } else {
+      alert("Wrong answer!");
+    }
+    // setTimeout(() => {
+
+    // }, 1000);
+    // checkForWinner();
+  });
+}
 // const findAnswer = () => {
 //   const answerArr = document.querySelectorAll(".answer-button");
 //   const correctAns = document.querySelector(".answer1");
-//   let userScore = 0;
-//   let revealPic = (document.querySelector("#pokemon-img").style.property ="brightness= 100%");
-//   const userInput = answerArr.addEventListner("click", event => {
-//     console.log(event.target.value);
-//   });
-//   if (userInput === correctAns) {
-//     userScore += 1;
+//   let revealPic = (document.querySelector("#pokemon-img").style.property =
+//     "brightness= 100%");
+//   if (answerArr.length === correctAns) {
+//     alert(`You got it! Well done. `);
 //     revealPic;
-//     alert(`You got it! You have ${userScore} now. `);
-//   } else if (userInput != correctAns) {
+//   } else {
 //     alert("Sorry, that was incorrect");
 //     revealPic;
 //   }
-//   if (userScore === 8 && 10) {
-//     alert(" You, Win! Go back to the Home Page and Try Again!");
-//     revealPic;
-//   } else {
-//     alert("You've lost the game, you're not a Pokemon Master.");
-//     revealPic;
-//   }
+//   findAnswer();
+
+// const userInput = answerArr.addEventListner("click", event => {
+//   console.log(event.target.value);
+// });
+// if (userInput === correctAns) {
+//   userScore += 1;
+//   revealPic;
+//   alert(`You got it! You have ${userScore} now. `);
+// } else if (userInput != correctAns) {
+//   alert("Sorry, that was incorrect");
+//   revealPic;
+// }
+// if (userScore === 8 && 10) {
+//   alert(" You, Win! Go back to the Home Page and Try Again!");
+//   revealPic;
+// } else {
+//   alert("You've lost the game, you're not a Pokemon Master.");
+//   revealPic;
+// }
 // };
+let checkForWinner = () => {
+  let winner = document.querySelectorAll(".duck");
+  if (winner.length === 1) {
+    alert("You've won, there are no more ducks");
+  }
+};
